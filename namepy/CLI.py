87a1813b349095c77ec8311e01d900548d5fcfc3
antlet <antlet@allegheny.edu>
2022-10-03 10:46:24 -0400

    Add basic report command as placeholder

--- namepy/CLI.py
+++ namepy/CLI.py
@@ -19,5 +19,13 @@ def find_var_len(directory_path: str):
     """Find the length of each assignment statement tuple (temp)."""
     var_len_dict = find.var_length(directory_path)
 
+@app.command()
+def report(directory_path: str):
+    """Find the length of each assignment statement tuple (temp)."""
+    var_dict = find.find_vars(directory_path)
+    print("\nNumber of variables per file:\n" + str(var_dict))
+    print("\nVariable name legnths:")
+    var_len_dict = find.var_length(directory_path)
+
 if __name__ == "__main__":
     app()
