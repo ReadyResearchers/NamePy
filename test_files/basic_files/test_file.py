25d8bebd8f5ebaef2eb104174b4645541a970969
antlet <antlet@allegheny.edu>
2022-10-25 00:58:43 -0400

    Add examples to test file

--- test_files/basic_files/test_file.py
+++ test_files/basic_files/test_file.py
@@ -3,33 +3,14 @@ import math
 
 variable01 = 0
 
-def greet(name):
+def greet_user(name):
     """Greet a user by name."""
     greeting = f"Hello, {name}!"
+    x = 1
+    xy = 2
+    xyz = 3
+    this_variable_is_definitely_too_long = 0
     print(greeting)
-
-
-def greet1(name):
-    """Greet a user by name."""
-    greeting1 = f"Hello, {name}!"
-    print(greeting1)
-
-
-def greet2(name):
-    """"Greet a user by name."""
-    greeting2 = f"Hello, {name}!"
-    print(greeting2)
-
-
-def greet3(name):
-    greeting2 = f"Hello, {name}!"
-    print(greeting2)
-
-
-def find_square_root():
-    square_root = math.sqrt(25)
-    print(square_root)
-
 
 if __name__ == "__main__":
     greet("World")
