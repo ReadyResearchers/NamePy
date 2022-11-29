3924334f72d705563e7a67daf18c5e80b2597017
antlet <antlet@allegheny.edu>
2022-11-29 14:35:13 -0500

    Modify test file

--- test_files/basic_files/test_file.py
+++ test_files/basic_files/test_file.py
@@ -1,24 +1,37 @@
 """This module implements the ability to greet users by name."""
-import math
 
-# class uwu:
+class Uwu:
+    """uwu"""
 
-#     variable01 = 0
+    variable01 = 0
 
-# class this_class_name_is_way_too_long:
+class ThisClassNameIsWayTooLongForAClassName:
 
-def this_function_name_is_too_long(this_parameter_is_way_too_long):
-    """Greet a user by name."""
-    greeting = f"Hello, {name}!"
-    x = 1
-    this_variable_is_definitely_too_long = 0
-    print(greeting)
+    def this_function_name_is_too_long(this_parameter_is_way_too_long, hello):
+        """docstring this_parameter_is_way_too_long."""
+        greeting = f"Hello, {this_parameter_is_way_too_long}!"
+        # Comment#2
+        # Ignore 'x'
+        # This comment has x in it
+        x = 1 # This comment has x in it
+        this_variable_is_definitely_too_long = 0
+        this_variable_is_definitely_too_long1 = 0
+        this_variable_is_definitely_too_long2 = 0
+        this_variable_is_definitely_too_long3 = 0
+        print(greeting)
 
 
-def x(arg):
-    """Greet a user by name."""
-    greeting = f"Hello, {arg}!"
-    print(greeting)
+    def Cou(arg):
+        """This is the docstring for Cou"""
+        greeting = f"Hello, {arg}!"
+        print(greeting)
 
-if __name__ == "__main__":
-    x("World")
+
+    # import spacy
+
+    # nlp = spacy.load("en_core_web_sm")
+    # doc = nlp("walk")
+
+    # print(f"{'text':{8}} {'POS':{6}} {'TAG':{6}} {'Dep':{6}} {'POS explained':{20}} {'tag explained'} ")
+    # for token in doc:
+    #     print(f'{token.text:{8}} {token.pos_:{6}} {token.tag_:{6}} {token.dep_:{6}} {spacy.explain(token.pos_):{20}} {spacy.explain(token.tag_)}')
