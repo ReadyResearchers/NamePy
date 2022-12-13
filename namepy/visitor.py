efa4b69ff039c95a02d9cfa14ec315c472dcc6c2
antlet <antlet@allegheny.edu>
2022-12-13 06:27:30 -0500

    Update visitor.py

--- namepy/visitor.py
+++ namepy/visitor.py
@@ -1,16 +1,11 @@
-# TODO: Add module docstring
-"""Add docstring here"""
+"""This module visits 4 types of identifiers to find specific linting errors."""
 
 import re
 import json
+import os
+import sys
 import libcst as cst
 import spacy
-
-# TODO: Create test cases
-# TODO: Handle directories
-# TODO: Make exit code 1 for failures
-# TODO: Make flag to ignore line
-# TODO: Dump jsons into new file(s)
 
 NLP = spacy.load("en_core_web_sm")
 
@@ -32,12 +27,6 @@ class IdentifierVisitor(cst.CSTVisitor):
             "ClassDef": 0,
             "Parameter": 0,
         }
-        self.possible_issues = {
-            "Assign": 0,
-            "FunctionDef": 0,
-            "ClassDef": 0,
-            "Parameter": 0,
-        }
         self.issue_frequency = {
             "Assign_Too_Long": 0,
             "Assign_Too_Short": 0,
@@ -54,20 +43,23 @@ class IdentifierVisitor(cst.CSTVisitor):
         }
 
     def visit_Assign(self, node: cst.Assign) -> None:
+        """Visit all instances of assignment statements."""
         parent = self.get_metadata(cst.metadata.ParentNodeProvider, node)
         for target in node.targets:
-            lookup = target.target.value
-            pos = self.get_metadata(cst.metadata.PositionProvider, target.target).start
-            line_num = pos.line
-            column = pos.column
-            self.identifier_frequency[("Assign")] +=1
-            self.possible_issues[('Assign')] += 3
-            # !look = target.target.value if str else blah blah
-            if lookup and lookup == str:
+            assign_target = target.target
+            # Only look at Name type objects (no Attributes)
+            if isinstance(assign_target, cst.Name):
+                lookup = assign_target.value
+                pos = self.get_metadata(
+                    cst.metadata.PositionProvider, target.target
+                ).start
+                line_num = pos.line
+                column = pos.column
+                self.identifier_frequency[("Assign")] += 1
                 if (len(lookup)) >= 31:
                     print(
                         f"{self.current_file_path}:{line_num}:{column}: Variable '{lookup}' "
-                        f"is of length {len(lookup)} -- Consider reducing length."
+                        f"is of length {len(lookup)} -- Reduce length."
                     )
                     self.issue_frequency[("Assign_Too_Long")] += 1
                 if (len(lookup)) <= 3:
@@ -84,10 +76,10 @@ class IdentifierVisitor(cst.CSTVisitor):
 
                 # *Python variables need to be in snake_case
                 # Split identifiers by their underscores
-                if '_' in lookup:
+                if "_" in lookup:
                     split_name_list = re.split("_+", lookup)
-                # Search for noun POS in list
-                # Throw error if no noun is found
+                    # Search for noun POS in list
+                    # Throw error if no noun is found
                     found_noun = False
                     for name in split_name_list:
                         parsed_text = NLP(name)
@@ -98,23 +90,23 @@ class IdentifierVisitor(cst.CSTVisitor):
                             break
                     if not found_noun:
                         print(
-                            f"{self.current_file_path}:{line_num}:{column}: Error with variable '{lookup}'"
-                            f"-- Consider using a noun."
+                            f"{self.current_file_path}:{line_num}:{column}: (Warning) with "
+                            f"variable '{lookup}' -- Consider using a noun."
                         )
                         self.issue_frequency[("Assign_No_Noun")] += 1
 
     def visit_FunctionDef(self, node: cst.FunctionDef) -> None:
+        """Visit all instances of function definitions."""
         function_docstring = node.get_docstring()
         lookup = node.name.value
         pos = self.get_metadata(cst.metadata.PositionProvider, node.name).start
         line_num = pos.line
         column = pos.column
-        self.identifier_frequency[("FunctionDef")] +=1
-        self.possible_issues[('FunctionDef')] += 3
+        self.identifier_frequency[("FunctionDef")] += 1
         if (len(lookup)) >= 31:
             print(
                 f"{self.current_file_path}:{line_num}:{column}: Function '{lookup}' "
-                f"is of length {len(lookup)} -- Consider reducing length."
+                f"is of length {len(lookup)} -- Reduce length."
             )
             self.issue_frequency[("Function_Too_Long")] += 1
         if (len(lookup)) <= 3:
@@ -123,7 +115,7 @@ class IdentifierVisitor(cst.CSTVisitor):
             else:
                 print(
                     f"{self.current_file_path}:{line_num}:{column}: Function '{lookup}' "
-                    f"is of length {len(lookup)} -- Add a comment or increase length."
+                    f"is of length {len(lookup)} -- Add to docstring or increase length."
                 )
                 self.issue_frequency[("Function_Too_Short")] += 1
         # *Python functions need to be in snake_case
@@ -141,23 +133,23 @@ class IdentifierVisitor(cst.CSTVisitor):
                 break
         if not found_verb:
             print(
-                f"{self.current_file_path}:{line_num}:{column}: Error with function '{lookup}' "
+                f"{self.current_file_path}:{line_num}:{column}: (Warning) with function '{lookup}' "
                 f"-- Consider using a verb."
             )
             self.issue_frequency[("Function_No_Verb")] += 1
 
     def visit_ClassDef(self, node: cst.ClassDef) -> None:
+        """Visit all instances of class definitions."""
         class_docstring = node.get_docstring()
         lookup = node.name.value
         pos = self.get_metadata(cst.metadata.PositionProvider, node.name).start
         line_num = pos.line
         column = pos.column
-        self.identifier_frequency[("ClassDef")] +=1
-        self.possible_issues[('ClassDef')] += 3
+        self.identifier_frequency[("ClassDef")] += 1
         if (len(lookup)) >= 31:
             print(
                 f"{self.current_file_path}:{line_num}:{column}: Class '{lookup}' "
-                f"is of length {len(lookup)} -- Consider reducing length."
+                f"is of length {len(lookup)} -- Reduce length."
             )
             self.issue_frequency[("Class_Too_Long")] += 1
         if (len(lookup)) <= 3:
@@ -166,7 +158,7 @@ class IdentifierVisitor(cst.CSTVisitor):
             else:
                 print(
                     f"{self.current_file_path}:{line_num}:{column}: Class '{lookup}' "
-                    f"is of length {len(lookup)} -- Add a comment or increase length."
+                    f"is of length {len(lookup)} -- Add to docstring or increase length."
                 )
                 self.issue_frequency[("Class_Too_Short")] += 1
         # *Python classes need to be in PascalCase
@@ -184,28 +176,28 @@ class IdentifierVisitor(cst.CSTVisitor):
                 break
         if not found_noun:
             print(
-                f"{self.current_file_path}:{line_num}:{column}: Error with class '{lookup}' "
+                f"{self.current_file_path}:{line_num}:{column}: (Warning) with class '{lookup}' "
                 f"-- Consider using a noun."
             )
             self.issue_frequency[("Class_No_Noun")] += 1
 
     def visit_Parameters(self, node: cst.Parameters) -> None:
+        """Visit all instances of function parameters."""
         parent = self.get_metadata(cst.metadata.ParentNodeProvider, node)
-        try:
+        if isinstance(parent, cst.FunctionDef):
             parameter_docstring = parent.get_docstring()
-        except(AttributeError):
+        else:
             return
         for param in node.params:
             lookup = param.name.value
             pos = self.get_metadata(cst.metadata.PositionProvider, param).start
             line_num = pos.line
             column = pos.column
-            self.identifier_frequency[("Parameter")] +=1
-            self.possible_issues[('Parameter')] += 3
+            self.identifier_frequency[("Parameter")] += 1
             if (len(lookup)) >= 31:
                 print(
                     f"{self.current_file_path}:{line_num}:{column}: Parameter '{lookup}' "
-                    f"is of length {len(lookup)} -- Consider reducing length."
+                    f"is of length {len(lookup)} -- Reduce length."
                 )
                 self.issue_frequency[("Param_Too_Long")] += 1
             if (len(lookup)) <= 3:
@@ -214,7 +206,7 @@ class IdentifierVisitor(cst.CSTVisitor):
                 else:
                     print(
                         f"{self.current_file_path}:{line_num}:{column}: Parameter '{lookup}' "
-                        f"is of length {len(lookup)} -- Add a comment or increase length."
+                        f"is of length {len(lookup)} -- Add to docstring or increase length."
                     )
                     self.issue_frequency[("Param_Too_Short")] += 1
             # *Python parameters need to be in snake_case
@@ -232,32 +224,36 @@ class IdentifierVisitor(cst.CSTVisitor):
                     break
             if not found_noun:
                 print(
-                    f"{self.current_file_path}:{line_num}:{column}: Error with parameter '{lookup}' "
-                    f"-- Consider using a noun."
+                    f"{self.current_file_path}:{line_num}:{column}: (Warning) with parameter "
+                    f"'{lookup}' -- Consider using a noun."
                 )
                 self.issue_frequency[("Param_No_Noun")] += 1
 
 
-FILE_PATH = "test_files/basic_files/test_file.py"
-# FILE_PATH = "namepy/visitor.py"
-# FILE_PATH = "test_files/gatorgrader_files/arguments.py"
-with open(FILE_PATH, "r", encoding="utf-8") as infile:
-    data = infile.read()
-wrapper = cst.metadata.MetadataWrapper(cst.parse_module(data))
-# print((cst.parse_module(data)))
-my_visitor = IdentifierVisitor(FILE_PATH)
-result = wrapper.visit(my_visitor)
-# Dump report dictionaries to jsons
-issue_report = json.dumps(my_visitor.issue_frequency, indent=4)
-identifier_report = json.dumps(my_visitor.identifier_frequency, indent=4)
-possible_issue_report = json.dumps(my_visitor.possible_issues, indent=4)
-# Find sum of report dictionary values
-issue_sum = sum(my_visitor.issue_frequency.values())
-identifier_sum = sum(my_visitor.identifier_frequency.values())
-possible_issue_sum = sum(my_visitor.possible_issues.values())
-# Calculate score out of 10
-possible_issue_quotient = possible_issue_sum / 10
-error_amount = issue_sum / possible_issue_quotient
-correct_amount = 10 - error_amount
-FORMAT_FLOAT = "{:.2f}".format(correct_amount)
-print(f"\nYour code has been rated a {FORMAT_FLOAT}/10.00\n")
+print("\033[1m")
+print("Running NamePy")
+print("\033[0m")
+FILE_PATH = sys.argv[1]
+
+if os.path.exists(FILE_PATH):
+    with open(FILE_PATH, "r", encoding="utf-8") as infile:
+        data = infile.read()
+    wrapper = cst.metadata.MetadataWrapper(cst.parse_module(data))
+    # print((cst.parse_module(data)))
+    my_visitor = IdentifierVisitor(FILE_PATH)
+    result = wrapper.visit(my_visitor)
+    # Dump report dictionaries to jsons
+    issue_report = json.dumps(my_visitor.issue_frequency, indent=4)
+    identifier_report = json.dumps(my_visitor.identifier_frequency, indent=4)
+    # Find sum of report dictionary values
+    issue_sum = sum(my_visitor.issue_frequency.values())
+    identifier_sum = sum(my_visitor.identifier_frequency.values()) * 2
+    # Calculate score out of 10
+    possible_issue_quotient = identifier_sum / 10
+    error_amount = issue_sum / possible_issue_quotient
+    correct_amount = 10 - error_amount
+    FORMAT_FLOAT = "{:.2f}".format(correct_amount)
+    print(f"\nYour code has been rated a {FORMAT_FLOAT}/10.00\n")
+    # print(identifier_report, issue_report)
+else:
+    print("Error: File path invalid\n")
