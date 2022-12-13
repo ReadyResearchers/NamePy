d956edf5fe52ec7103d2c9864d504959dd1c2608
antlet <antlet@allegheny.edu>
2022-12-13 06:27:13 -0500

    Add test files

---
+++ test-files/test_file.py
@@ -0,0 +1,47 @@
+"""This module implements the ability to greet users by name."""
+
+# class Uwu:
+#     """uwu"""
+
+    # variable01 = 0
+
+class ThisClassNameIsWayTooLongForAClassName:
+
+    def function_one(this_parameter_is_way_too_long, hello):
+        """docstring this_parameter_is_way_too_long."""
+        greeting = f"Hello, {this_parameter_is_way_too_long}!"
+        # Comment#2
+        # Ignore 'x'
+        # This comment has x in it
+
+        x = 1
+        # y = 2
+        this_variable_is_definitely_too_long = 0
+        # this_variable_is_definitely_too_long1 = 0
+        # this_variable_is_definitely_too_long2 = 0
+        # this_variable_is_definitely_too_long3 = 0
+        print(greeting)
+
+
+    def Cou(arg):
+        """This is the docstring for Cou"""
+        greeting = f"Hello, {arg}!"
+        print(greeting)
+
+    # def func1():
+    #     x = 12
+    #     y = 29
+    #     z = x * y
+
+        print(z)
+
+
+
+    # import spacy
+
+    # nlp = spacy.load("en_core_web_sm")
+    # doc = nlp("walk")
+
+    # print(f"{'text':{8}} {'POS':{6}} {'TAG':{6}} {'Dep':{6}} {'POS explained':{20}} {'tag explained'} ")
+    # for token in doc:
+    #     print(f'{token.text:{8}} {token.pos_:{6}} {token.tag_:{6}} {token.dep_:{6}} {spacy.explain(token.pos_):{20}} {spacy.explain(token.tag_)}')
---
+++ test-files/test_file01.py
@@ -0,0 +1,6 @@
+def find_area( ):
+	length = 12
+	width = 29
+	area = length * width
+
+	print(area)
